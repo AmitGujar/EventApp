a3b4d051dd819638bfecdcb5ebf5f4421ed6e885
AmitGujar <amithero3342@gmail.com>
2022-12-28 00:48:41 +0530

    db connected

--- app/src/main/java/com/example/eventapp/Register.java
+++ app/src/main/java/com/example/eventapp/Register.java
@@ -1,8 +1,8 @@
 package com.example.eventapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -12,38 +12,40 @@ import android.widget.Toast;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-
-public class Login extends AppCompatActivity {
+public class Register extends AppCompatActivity {
     DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReferenceFromUrl("https://eventapp-fb89f-default-rtdb.firebaseio.com/");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         final EditText usernameET = findViewById(R.id.usernameET);
         final EditText passwordET = findViewById(R.id.passwordET);
-        final TextView signUpBtn = findViewById(R.id.signUpBtn);
+        final AppCompatButton signUpBtn = findViewById(R.id.signUpBtn);
         final TextView signInBtn = findViewById(R.id.signInBtn);
-
-        signInBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String username = usernameET.getText().toString();
-                String password = passwordET.getText().toString();
-//                databaseReference.child("Users").child(username).child("Password").setValue(password);
-                if (username.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(Login.this, "Please enter all the details", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
 
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Login.this, Register.class));
+                String username = usernameET.getText().toString();
+                String password = passwordET.getText().toString();
+
+                if (username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(Register.this, "Please enter all the details", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    databaseReference.child("Users").child(username).child("Password").setValue(password);
+                    Toast.makeText(Register.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        });
+
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
