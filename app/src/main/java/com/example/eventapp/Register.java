97223d271331de1231bb6d275ad8769bfcac4be4
AmitGujar <amithero3342@gmail.com>
2022-12-28 14:42:46 +0530

    check username

--- app/src/main/java/com/example/eventapp/Register.java
+++ app/src/main/java/com/example/eventapp/Register.java
@@ -1,16 +1,23 @@
 package com.example.eventapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import es.dmoral.toasty.Toasty;
 
@@ -25,6 +32,23 @@ public class Register extends AppCompatActivity {
         final EditText usernameET = findViewById(R.id.usernameET);
         final EditText passwordET = findViewById(R.id.passwordET);
         final AppCompatButton signUpBtn = findViewById(R.id.signUpBtn);
+        final ImageView passwordIcon = findViewById(R.id.passwordIcon);
+
+        passwordIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (passwordET.getTransformationMethod() == null) {
+                    passwordET.setTransformationMethod(new android.text.method.PasswordTransformationMethod());
+                    Drawable drawable = AppCompatResources.getDrawable(Register.this, R.drawable.password_hide);
+                    passwordIcon.setImageDrawable(drawable);
+                }
+                else {
+                    passwordET.setTransformationMethod(null);
+                    Drawable drawable = AppCompatResources.getDrawable(Register.this, R.drawable.password_show);
+                    passwordIcon.setImageDrawable(drawable);
+                }
+            }
+        });
 
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,11 +60,27 @@ public class Register extends AppCompatActivity {
                 if (username.isEmpty() || password.isEmpty()) {
                     Toasty.info(Register.this, "Please enter all the details", Toast.LENGTH_SHORT).show();
                 }
+
                 else {
-                    databaseReference.child("Users").child(username).child("Full Name").setValue(name);
-                    databaseReference.child("Users").child(username).child("Password").setValue(password);
-                    Toasty.success(Register.this, "Registration Successful", Toast.LENGTH_SHORT).show();
-                    finish();
+                    databaseReference.child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if (snapshot.hasChild(username)) {
+                                Toasty.info(Register.this, "Username already exists", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                databaseReference.child("Users").child(username).child("Full Name").setValue(name);
+                                databaseReference.child("Users").child(username).child("Password").setValue(password);
+                                Toasty.success(Register.this, "Account created successfully", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            Toasty.error(Register.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }
             }
         });
